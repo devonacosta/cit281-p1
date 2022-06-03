55e9f75d16877f15850afec601b3351b47b92395
devonacosta <105311604+devonacosta@users.noreply.github.com>
2022-06-03 13:25:16 -0700

    Add files via upload

---
+++ p1-date.js
@@ -0,0 +1,13 @@
+/* 
+    CIT 281 Project 1
+    Name :Devon Acosta
+*/
+
+let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//input array somewhere
+let today = new Date();
+//use date object to help get the date of week
+let weekDay = today.toString(daysOfWeek);
+// find a syntax for representing the object 
+    console.log(weekDay);
+    //need output
